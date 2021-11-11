2e319777816794e139820ac157d2c6e09174f705
Tadhg White <tadhg.white.2017@mumail.ie>
2021-11-11 15:26:33 +0000

    Unsure gh-pages deploys .nojekyll Remove redundant slug-fixing code

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -12,18 +12,7 @@
 </script>
 
 <script>
-    import { base } from "$app/paths";
     export let projects;
-
-    function fixSlug(slug) {
-        // return `${base}/${slug}`;
-        // console.log(`Base path at this moment: ${base}`);
-        // console.log(`Slug at this moment: ${slug}`);
-        // console.log(`Theoretical full path at this moment: ${base}/${slug}`);
-        // console.log(`Possible fix: ${base}/${slug.slice("projects".length)}`);
-
-        return slug
-    }
 </script>
 
 <svelte:head>
@@ -35,11 +24,11 @@
 <ul class="py-2 space-y-2">
     {#each projects as { title, summary, languages, tools, slug }}
         <li class="border-2 border-purple-600 dark_border-purple-400 p-2 max-w-prose">
-            <h2><a sveltekit:prefetch href={fixSlug(slug)}>{title}</a></h2>
+            <h2><a sveltekit:prefetch href={slug}>{title}</a></h2>
             <p><span class="font-semibold">Language(s)</span>: {languages.join(", ")}</p>
             <p><span class="font-semibold">Tools/Frameworks:</span> {tools.join(", ")}</p>
             <p>{summary}</p>
-            <a sveltekit:prefetch href={fixSlug(slug)}>Read More &gt;&gt;</a>
+            <a sveltekit:prefetch href={slug}>Read More &gt;&gt;</a>
         </li>
     {/each}
 </ul>
