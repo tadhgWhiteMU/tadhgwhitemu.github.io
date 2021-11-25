0e43d18c047e4d1e3229b35fe635ffdcd993f13a
Tadhg White <tadhg.white.2017@mumail.ie>
2021-11-25 11:51:45 +0000

    Add anchor tags to Link component

--- src/lib/Link/index.svelte
+++ src/lib/Link/index.svelte
@@ -5,11 +5,11 @@
 </script>
 
 {#if isExternal}
-    <a href="{url}" title="{title}" referrerpolicy="no-referrer" target="_blank" rel="external noopener">
+    <a href={url} {title} referrerpolicy="no-referrer" target="_blank" rel="external noopener">
         <slot />
     </a>
 {:else}
-    <a sveltekit:prefetch href="{url}" title="{title}">
+    <a sveltekit:prefetch href={url} {title}>
         <slot />
     </a>
 {/if}
